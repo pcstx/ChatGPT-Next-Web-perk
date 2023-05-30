819961663461c163248c54ac52d461bc156c253c
陈大人 <chensiyuan@smt-h.cn>
2023-05-30 16:52:32 +0800

    分析图片

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -4,7 +4,7 @@ import styles from "./home.module.scss";
 
 import { IconButton } from "./button";
 import SettingsIcon from "../icons/settings.svg";
-import GithubIcon from "../icons/github.svg";
+import PIcon from "../icons/p.svg";
 import ChatGptIcon from "../icons/chatgpt.svg";
 import AddIcon from "../icons/add.svg";
 import CloseIcon from "../icons/close.svg";
@@ -20,7 +20,7 @@ import {
   MIN_SIDEBAR_WIDTH,
   NARROW_SIDEBAR_WIDTH,
   Path,
-  REPO_URL,
+  PUSHPLUS_URL,
 } from "../constant";
 
 import { Link, useNavigate } from "react-router-dom";
@@ -171,11 +171,11 @@ export function SideBar(props: { className?: string }) {
               <IconButton icon={<SettingsIcon />} shadow />
             </Link>
           </div>
-          {/* <div className={styles["sidebar-action"]}>
-            <a href={REPO_URL} target="_blank">
-              <IconButton icon={<GithubIcon />} shadow />
+          <div className={styles["sidebar-action"]}>
+            <a href={PUSHPLUS_URL} target="_blank">
+              <IconButton icon={<PIcon />} shadow />
             </a>
-          </div> */}
+          </div>
         </div>
         <div>
           <IconButton
