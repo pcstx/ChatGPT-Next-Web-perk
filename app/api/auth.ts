6e80dc123e62919ca900824907a333c0e9848bfb
jandy.sang <jandy.sang@ly.com>
2023-06-09 13:56:31 +0800

    处理免费额度

--- app/api/auth.ts
+++ app/api/auth.ts
@@ -2,7 +2,13 @@ import { NextRequest } from "next/server";
 import { getServerSideConfig } from "../config/server";
 import md5 from "spark-md5";
 import { ACCESS_CODE_PREFIX } from "../constant";
-import { OPENAI_URL } from "./common";
+import { LRUCache } from "lru-cache";
+// import { OPENAI_URL } from "./common";
+
+const cache = new LRUCache({
+  max: 500, // 最多缓存500个条目
+  ttl: 1000 * 60 * 30, // 每个条目最长缓存30分钟
+});
 
 function getIP(req: NextRequest) {
   let ip = req.ip ?? req.headers.get("x-real-ip");
@@ -23,6 +29,53 @@ function parseApiKey(bearToken: string) {
     accessCode: isOpenAiKey ? "" : token.slice(ACCESS_CODE_PREFIX.length),
     apiKey: isOpenAiKey ? token : "",
   };
+}
+
+/**
+ * 处理pushplus的token登录状态
+ * 返回：0:未登录，1:免费会员不可用，2:免费会员可用，3:会员，
+ */
+async function handlerPerkAuth(perkToken: string) {
+  if (perkToken && perkToken.length) {
+    const isVip: boolean = !!cache.get(`pp-${perkToken}`);
+    if (!isVip) {
+      const res = await fetch(
+        "https://www.pushplus.plus/api/customer/user/chatGPT",
+        {
+          headers: {
+            pushToken: perkToken,
+          },
+        },
+      );
+      const response = await res.json();
+      const isFreeUser = response?.data?.freeUse ?? false;
+      const isVip = !!(response?.data?.isVip ?? 0);
+      if (isVip) {
+        cache.set(`pp-${perkToken}`, "1");
+        return 3;
+      }
+      if (isFreeUser) {
+        return 2;
+      } else {
+        return 1;
+      }
+    } else {
+      //读取缓存
+      return 3;
+    }
+  } else {
+    console.log("[Auth] need sign in pushplus");
+    return 0;
+  }
+}
+async function getPerkVip(perkToken: string) {
+  //私有化方法不做太多的判断了，直接出发方法就好了
+
+  return await fetch("https://www.pushplus.plus/api/customer/user/chatGPT", {
+    headers: {
+      pushToken: perkToken,
+    },
+  });
 }
 
 export async function auth(req: NextRequest) {
@@ -48,7 +101,6 @@ export async function auth(req: NextRequest) {
   }
 
   // if user does not provide an api key, inject system api key
-  console.log("token:" + token);
   if (!token) {
     const apiKey = serverConfig.apiKey;
     if (apiKey) {
@@ -57,36 +109,22 @@ export async function auth(req: NextRequest) {
     } else {
       console.log("[Auth] admin did not provide an api key");
     }
-
-    if (req.cookies.get("pushToken")?.value) {
-      //请求判断是否会员
-      const res = await fetch(
-        "http://www.pushplus.plus/api/customer/vipUser/isVip",
-        {
-          headers: {
-            pushToken: req.cookies.get("pushToken")?.value || "",
-          },
-        },
-      );
-      const response = await res.json();
-      if (response.code === 302) {
+    const perkAuthType = await handlerPerkAuth(
+      req.cookies.get("pushToken")?.value ?? "",
+    );
+    if (perkAuthType != 3 && perkAuthType != 2) {
+      if (perkAuthType == 0) {
         return {
           customError: true,
           msg: "请先登录[pushplus](//www.pushplus.plus/login.html?backUrl=https://ai.pushplus.plus)或在[设置](/#/settings)中输入API Key",
         };
       }
-      if (response?.data?.isVip != 1) {
+      if (perkAuthType == 1) {
         return {
           customError: true,
-          msg: "此功能仅供会员使用，点击[开通会员](//www.pushplus.plus/vip.html)或在[设置](/#/settings)中输入API Key",
+          msg: "免费额度使用完毕，点击[开通会员](//www.pushplus.plus/vip.html)或在[设置](/#/settings)中输入API Key",
         };
       }
-    } else {
-      console.log("没有pushToken");
-      return {
-        customError: true,
-        msg: "请先登录[pushplus](//www.pushplus.plus/login.html?backUrl=https://ai.pushplus.plus)或在[设置](/#/settings)中输入API Key",
-      };
     }
   } else {
     console.log("[Auth] use user api key");
