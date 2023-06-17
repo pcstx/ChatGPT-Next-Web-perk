00debe4b4451177e51bedaeed600adce74a8d15b
陈大人 <chensiyuan@pushplus.plus>
2023-06-17 14:58:53 +0800

    修改配置

--- public/tj.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?edd03dfe328c1e0d79e353535640a03b";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
