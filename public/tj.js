2b5502575d45601684681e953666f7944d92e0ad
陈大人 <chensiyuan@smt-h.cn>
2023-06-20 14:54:53 +0800

    统计

---
+++ public/tj.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?edd03dfe328c1e0d79e353535640a03b";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
